6ea4d4379f191ba1f369666ed8d23eefaf26788b
026-Ink <1011268631@qq.com>
2021-07-22 19:59:56 +0800

    Writing filesystem.

--- include/fs.h
+++ include/fs.h
@@ -105,6 +105,8 @@ public:
 
 class Directory {
 private:
+  Directory* m_parent; // parent directory
+
   std::vector<File*> m_sub_files; // subfiles of this directory
   std::vector<Directory*> m_sub_dirs; // subdirectories of this directory
   std::string m_name; // name of directory
@@ -118,15 +120,15 @@ private:
 
 private:
   // NAME:
-  //   AuxiliaryRemoveAll
+  //   Removing
   // PARAMS:
   //   None
   // RETURNS:
   //   None
   // DESCRIPTION:
-  //   a auxiliary function, to remove all data from this directory.
+  //   a auxiliary function. to removes all data from this directory.
   void
-  AuxiliaryRemoveAll();
+  Remove();
 
 public:
   // NAME:
@@ -140,32 +142,93 @@ public:
   // NAME:
   //   Directory
   // PARAMS:
-  //   name: std::string, uid: int
+  //   name: std::string, uid: int, prt: Directory* (default = nullptr)
   // DESCRIPTION:
   //   sets 'm_name', 'm_uid'.
-  Directory(std::string name, int uid) :
-    m_name{name}, m_uid{uid}
+  Directory(std::string name, int uid, Directory* prt = nullptr) :
+    m_name{name}, m_uid{uid}, m_parent{prt}
   {}
 
   // NAME:
   //   Directory
   // PARAMS:
-  //   src: const Directory&
+  //   src: const Directory&, uid: int, prt: Directory* (default = nullptr)
   // DESCRIPTION:
   //   copies all members from 'src' and sets 'm_uid'.
-  Directory(Directory src, int uid);
+  Directory(Directory src, int uid, Directory* prt = nullptr);
+
+  // NAME:
+  //   GetName
+  // PARAMS:
+  //   None
+  // RETURNS:
+  //   A C-Style string
+  // DESCRIPTION:
+  //   gets the name of directory.
+  constexpr const char*
+  GetName();
 
   // NAME:
   //   RemoveAll
   // PARAMS:
   //   executor_uid: int
   // RETURNS:
-  //   an integer, indicates whether executing is successful.
+  //   an integer, indicates whether executed was successful.
   // DESCRIPTION:
   //   recursively removes all subfiles and subdirectories.
   int
   RemoveAll(int executor_uid);
+
+  // NAME:
+  //   Create
+  // PARAMS:
+  //   name: std::string, executor_uid: int, type: int (default = 1)
+  // IMPORTANT PARAMS:
+  //   type: 1 indicates file, 2 indicates directory.
+  // RETURNS:
+  //   an integer, indicates whether executed was successful.
+  // DESCRIPTION:
+  //   creates a file or directory to this directory.
+  int
+  Create(std::string name, int executor_uid, int type = 1);
+
+  // NAME:
+  //   Remove
+  // PARAMS:
+  //   name: std::string, executor_uid: int
+  // NOTE:
+  //   the subfiles vector or subdirectories vector may be null.
+  //   if two vectors are null, or cannot finds the file (directory),
+  //   we return a special value: 1.
+  // RETURNS:
+  //   an integer, indicates whether executed was successful.
+  // DESCRIPTION:
+  //   removes a file or directory from this directory.
+  int
+  RemoveSingle(std::string name, int executor_uid);
 };
+
+class FileSystem {
+private:
+  Directory m_root; // the root directory of filesystem.
+  Directory* m_working; // working directory.
+
+public:
+  // TODO
+};
+
+// NAME:
+//   CheckPermission
+// PARAMS:
+//   need_check_uid: int, check_uid: int, is_given: bool
+// IMPORTANT PARAMS:
+//   is_given: indicates the permission has been given.
+// RETURNS:
+//   a boolean, indicates whether the user has permission to execute commands.
+// DESCRIPTION:
+//   check the permission, then transfer to other functions.
+bool
+CheckPermission(int need_check_uid, int check_uid, bool is_given);
 
 } // namespace hackbit::fs
 
--- src/fs.cc
+++ src/fs.cc
@@ -34,7 +34,7 @@ File::GetName() {
 
 // class 'Directory':
 
-Directory::Directory(Directory src, int uid) {
+Directory::Directory(Directory src, int uid, Directory* prt) {
   // copies members.
   this->m_name = src.m_name;
   this->m_sub_files = src.m_sub_files;
@@ -43,10 +43,12 @@ Directory::Directory(Directory src, int uid) {
   this->m_other_write = src.m_write;
   // sets uid.
   this->m_uid = uid;
+  // sets parent directory.
+  this->m_parent = prt;
 }
 
 void
-Directory::AuxiliaryRemoveAll() {
+Directory::Remove() {
   // if this directory's subfiles vector is not empty:
   if ( !this->m_sub_files.empty() ) {
     // traversing vector:
@@ -65,27 +67,105 @@ Directory::AuxiliaryRemoveAll() {
     // this directory is not a leaf node in directory tree:
     // recursively access subnode(s):
     for ( auto & a : this->m_sub_dirs ) {
-      a->AuxiliaryRemoveAll();
+      a->Remove();
     }
   }
+}
+
+constexpr const char *
+Directory::GetName() {
+  return this->m_name.c_str();
 }
 
 int
 Directory::RemoveAll(int executor_uid) {
   int uid = executor_uid;
 
-  // uid is 0 indicating executor is administrator.
-  if ( uid == 0 || uid == this->m_uid ) {
-    AuxiliaryRemoveAll();
+  // check permission.
+  auto has_permission = CheckPermission(this->m_uid, uid, this->m_write);
+
+  // if this user has write permission:
+  if ( has_permission ) {
+    this->Remove();
     return 0;
   }
 
-  // if other users can change this directory
-  if ( m_write ) {
-    AuxiliaryRemoveAll();
-    return 0;
-  }
   return -1; // oops: user has not permission to execute.
+}
+
+int
+Directory::Create(std::string name, int executor_uid, int type) {
+  int uid = executor_uid;
+
+  // check permission.
+  auto has_permission = CheckPermission(this->m_uid, uid, this->m_write);
+
+  // if this user has write permission:
+  if ( has_permission ) {
+    // if user want to create a file:
+    if ( type == 1 ) {
+      // create a object pointer and push back.
+      this->m_sub_files.push_back(new File(name, executor_uid));
+    }
+    // if user want to create a directory:
+    if ( type == 2 ) {
+      // create a object pointer and push back.
+      this->m_sub_dirs.push_back(new Directory(name, executor_uid));
+    }
+  }
+
+  return -1; // oops: user has not permission to execute.
+}
+
+int
+Directory::RemoveSingle(std::string name, int executor_uid) {
+  int uid = executor_uid;
+
+  // check permission.
+  auto has_permission = CheckPermission(this->m_uid, uid, this->m_write);
+
+  // if this user has write permission:
+  if ( has_permission ) {
+    // if not such file or directory: we return 1.
+    if ( this->m_sub_files.empty() && this->m_sub_dirs.empty() ) return 1;
+
+    // traversing subfiles vector:
+    for ( auto a = this->m_sub_files.begin(); a != this->m_sub_files.end(); ++a ) {
+      // if find this file:
+      if ( (*a)->GetName() == name ) {
+        // erase the pointer.
+        m_sub_files.erase(a);
+        // return successful value.
+        return 0;
+      }
+    }
+
+    // traversing subdirectories vector:
+    for ( auto a = this->m_sub_dirs.begin(); a != this->m_sub_dirs.end(); ++a ) {
+      // if find this directory:
+      if ( (*a)->GetName() == name ) {
+        // erase the pointer.
+        m_sub_dirs.erase(a);
+        // return successful value.
+        return 0;
+      }
+    }
+
+    return 1; // no such file or directory.
+  }
+
+  return -1; // oops: user has not permission to execute.
+}
+
+// other functions:
+bool
+CheckPermission(int need_check_uid, int check_uid, bool is_given) {
+  // if the check uid equals need check uid or check uid is 0 (Administrator):
+  if ( need_check_uid == check_uid || check_uid == 0 ) return true;
+  // if the permission was not give:
+  if ( !is_given ) return false;
+  // else, the permission has been given:
+  return true;
 }
 
 } // namespace hackbit::fs
